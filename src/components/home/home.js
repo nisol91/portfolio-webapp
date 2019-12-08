6af43a0177cf78869028a711340e99d1abc82a06
Nicola <39983382+nisol91@users.noreply.github.com>
2019-12-08 21:29:12 +0100

    connected to firestore

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,10 +7,25 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { translate } from "react-i18next";
 
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCW3XY0Dg3E27kaIFM4IfvITbl5pXyw3io",
+  authDomain: "portfolio-f8a45.firebaseapp.com",
+  databaseURL: "https://portfolio-f8a45.firebaseio.com",
+  projectId: "portfolio-f8a45",
+  storageBucket: "portfolio-f8a45.appspot.com",
+  messagingSenderId: "267374463312",
+  appId: "1:267374463312:web:bdcf27db9088ce82fb0455",
+  measurementId: "G-1F4M1LF7NV"
+});
+const db = firebaseApp.firestore();
+export { db };
+
 class Home extends Component {
   constructor(props) {
     super(props);
-
+    this.addProject = this.addProject.bind(this);
     this.state = {
       cubeVisibility: false
     };
@@ -24,7 +39,36 @@ class Home extends Component {
       this.setState({ cubeVisibility: true });
       this.notify();
     }, 1500);
+    this.fetchProjects();
   }
+
+  async fetchProjects() {
+    await db
+      .collection("projects")
+      .get()
+      .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        console.log("=========");
+        console.log(data);
+        console.log("=========");
+      });
+  }
+  addProject() {
+    console.log("inizio");
+
+    db.collection("projects")
+      .add({
+        name: " projectLos Angeles",
+        description: "bello"
+      })
+      .then(function() {
+        console.log("Document successfully written!");
+      })
+      .catch(function(error) {
+        console.error("Error writing document: ", error);
+      });
+  }
+
   render() {
     //==handling css classes==
     // let className_1 = "boxPortfolio";
@@ -42,6 +86,12 @@ class Home extends Component {
           <Link to={"/contact-me"} className="mylink">
             <div className="myBtnContact swing-in-top-fwd">{t("contacts")}</div>
           </Link>
+          <div
+            onClick={this.addProject}
+            className="myBtnContact swing-in-top-fwd"
+          >
+            add project
+          </div>
         </div>
 
         <div className={`fade-in ${this.state.cubeVisibility && "visible"}`}>
