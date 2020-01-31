c94e8054f7aa8d2262083a8833070ec85490dc17
Nicola <39983382+nisol91@users.noreply.github.com>
2020-01-31 09:53:29 +0100

    solved projects problem

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./project.scss";
 import { Link } from "react-router-dom";
 import { Spinner } from "reactstrap";
+import { db } from "../portfolio_single_page/portfolio_sp";
 
 export default class Project extends Component {
   constructor(props) {
@@ -9,9 +10,25 @@ export default class Project extends Component {
 
     this.state = {
       image: null,
-      imageLoaded: false
+      imageLoaded: false,
+      projectId: this.props.match.params,
+      project: []
     };
     this.handleImageLoaded = this.handleImageLoaded.bind(this);
+  }
+
+  async fetchProjects(projID) {
+    await db
+      .collection("projects")
+      .where("id", "==", projID)
+      .get()
+      .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        console.log("=====PROJECTS in project====");
+        console.log(data[0].img);
+        console.log("=====PROJECTS====");
+        this.setState({ project: data[0] });
+      });
   }
   handleImageLoaded() {
     setTimeout(() => {
@@ -22,25 +39,32 @@ export default class Project extends Component {
     }, 0);
   }
   componentDidMount() {
+    const { projID } = this.props.match.params;
+    this.fetchProjects(projID);
     console.log("====================================");
-    console.log(this.props.location.state);
+    // console.log(this.props.location.state);
+    console.log(projID);
+
     console.log("====================================");
   }
 
   render() {
     return (
       <div className="projCont">
+        <div className="titleProj">
+          <h1>{this.state.project.name}</h1>
+          <h3>{this.state.project.description}</h3>
+        </div>
         <div className="imgContainer">
           {!this.state.imageLoaded && (
             <Spinner color="primary" className="imgSpinner" />
           )}
           <img
-            src={this.props.location.state.img}
+            src={this.state.project.img}
             alt=""
             onLoad={this.handleImageLoaded}
           />
         </div>
-        <h1>{this.props.location.state.name}</h1>
       </div>
     );
   }
